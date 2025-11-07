4e5a59e30f869d238b318e51c48d14bb8b7be19c
khian calderon <ckhianvictory@gmail.com>
2025-11-07 20:15:52 +0800

    Updated id

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var searchInput = document.getElementById("text-input");
+var searchInput = document.getElementById("search-input");
 var selectInput = document.getElementById("select-input");
 var caseSensitivityInput = document.getElementById("case-sensitive-input");
 var wholeWordOnlyInput = document.getElementById("whole-word-only-input");
