d8accdff29a9a1412fc3942e4c9c339a43232e69
khian calderon <ckhianvictory@gmail.com>
2025-11-07 20:25:28 +0800

    Added table + input script

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,16 @@ var searchInput = document.getElementById("search-input");
 var selectInput = document.getElementById("select-input");
 var caseSensitivityInput = document.getElementById("case-sensitive-input");
 var wholeWordOnlyInput = document.getElementById("whole-word-only-input");
+
+const tableHeaders = `
+  <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Address</th>
+    <th>Birth Date</th>
+  </tr>
+`;
+
+searchInput.addEventListener("input", () => {
+  // Only trigger re-search if input was changed
+});
